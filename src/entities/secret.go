c540815ab77f21aab37e2ffe32d8f03ba093df17
Estevão Costa <ecosta@mpes.mp.br>
2021-08-12 15:22:29 -0300

    Add more secrets validate

--- src/entities/secret.go
+++ src/entities/secret.go
@@ -27,6 +27,22 @@ func (s *Secret) UnmarshalYAML(unmarshal func(interface{}) error) error {
 func ValidateSecretsFromAppDeploy(appDeployPath string, repoRules *RepositoryRules) error {
 	var globalErr *multierror.Error
 
+	// checking secrets name: must be the repository name
+	secretsName, err := infra.YqSearchQueryInFileWithStringSliceReturn(appDeployPath,
+		".spec.jobTemplate.spec.template.spec.containers[].env[].valueFrom.secretKeyRef.name,.spec.template.spec.containers[].env[].valueFrom.secretKeyRef.name")
+	if err != nil {
+		globalErr = multierror.Append(globalErr, err)
+	}
+	if len(secretsName) > 1 {
+		globalErr = multierror.Append(globalErr,
+			fmt.Errorf("more than one k8s secret by repository is not allowed. current k8s-secrets: %v", secretsName))
+	}
+	if len(secretsName) > 0 && secretsName[0] != repoRules.Name {
+		globalErr = multierror.Append(globalErr,
+			fmt.Errorf("the k8s-secret name must be the same as the repository name. current name: %s;expected: %s", secretsName[0], repoRules.Name))
+	}
+
+	// checking if all secrets was declared
 	secrets, err := infra.YqSearchQueryInFileWithStringSliceReturn(appDeployPath,
 		".spec.jobTemplate.spec.template.spec.containers[].env[].valueFrom.secretKeyRef.key,.spec.template.spec.containers[].env[].valueFrom.secretKeyRef.key")
 	if err != nil {
